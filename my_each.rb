22481241ae6cd30520d8b4e1bb0e550c4c8dd18d
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-12-15 23:34:13 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,11 @@
-def my_each (collection = [1,2,3,4])
-  number = 0
-while number<collection.length
+def my_each (collection)
+i = 1
+while i<collection.length+1
 yield i
+i +=1
 end
 end
+collection = [1,2,3,4]
 my_each(collection) do |i|
   puts i
 end
